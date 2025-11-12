f6b91cf10468b37a93d35c3c3f10596b74dfd526
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-11-12 12:02:49 +0900

    [준비] 로직 개선을 위한 밑준비

---
+++ routy-be/src/main/java/com/c4/routy/domain/direction/optimization/factory/RouteStrategyFactory.java
@@ -0,0 +1,4 @@
+package com.c4.routy.domain.direction.optimization.factory;
+
+public class RouteStrategyFactory {
+}
---
+++ routy-be/src/main/java/com/c4/routy/domain/direction/optimization/strategy/RouteStrategy.java
@@ -0,0 +1,4 @@
+package com.c4.routy.domain.direction.optimization.strategy;
+
+public interface RouteStrategy {
+}
